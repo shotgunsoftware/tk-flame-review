9f67f2f6c12226302179194def59c8846943282a
Kerby Geffrard <kerby.geffrard@autodesk.com>
2017-11-08 18:21:45 -0500

    Adjust movie name to the asset name

--- app.py
+++ app.py
@@ -13,12 +13,11 @@ Flame app to send sequences to review.
 """
 
 import os
-import sys
 import uuid
-import sgtk
-from sgtk import TankError
 
+from sgtk import TankError
 from sgtk.platform import Application
+
 
 class FlameReview(Application):
     """
@@ -26,7 +25,7 @@ class FlameReview(Application):
     
     Generates quicktimes for the selected Flame sequences and uploads these to Shotgun.
     """
-    
+
     def init_app(self):
         """
         Called as the application is being initialized.
@@ -35,13 +34,13 @@ class FlameReview(Application):
 
         # register our desired interaction with Flame hooks
         menu_caption = self.get_setting("menu_name")
-        
+
         # track the comments entered by the user
         self._review_comments = ""
-        
+
         # flag to indicate that something was actually submitted
         self._submission_done = False
-        
+
         # set up callbacks for the engine to trigger 
         # when this profile is being triggered 
         callbacks = {}
@@ -49,10 +48,10 @@ class FlameReview(Application):
         callbacks["preExportAsset"] = self.adjust_path
         callbacks["postExportAsset"] = self.populate_shotgun
         callbacks["postCustomExport"] = self.display_summary
-        
+
         # register with the engine
         self.engine.register_export_hook(menu_caption, callbacks)
-        
+
     def pre_custom_export(self, session_id, info):
         """
         Flame hook called before a custom export begins. The export will be blocked
@@ -73,31 +72,31 @@ class FlameReview(Application):
         """
         # Note - Since Flame is a PySide only environment, we import it directly
         # rather than going through the sgtk wrappers.         
-        from PySide import QtGui, QtCore
-        
+        from PySide import QtGui
+
         # clear our flags
         self._submission_done = False
-        
+
         # pop up a UI asking the user for description
         tk_flame_review = self.import_module("tk_flame_review")
         (return_code, widget) = self.engine.show_modal("Submit for Review", self, tk_flame_review.SubmitDialog)
-        
+
         if return_code == QtGui.QDialog.Rejected:
             # user pressed cancel
             info["abort"] = True
             info["abortMessage"] = "User cancelled the operation."
-        
+
         else:
             # get comments from user
             self._review_comments = widget.get_comments()
-            
+
             # populate the host to use for the export. Currently hard coded to local
             info["destinationHost"] = self.engine.get_server_hostname()
             # set the (temp) location where media is being output prior to upload.
             info["destinationPath"] = self.engine.get_backburner_tmp()
             # pick up the xml export profile from the configuration
             info["presetPath"] = self.execute_hook_method("settings_hook", "get_export_preset")
-    
+
             self.log_debug("%s: Starting custom export session with preset '%s'" % (self, info["presetPath"]))
 
         # Log usage metrics
@@ -149,16 +148,18 @@ class FlameReview(Application):
             # generated - but just in case there are (because of customizations), we'll simply
             # ignore these.
             return
-        
+
+        name = info.get("assetName", info.get("name"))  # ensure backward compatibility
+
         # ensure each quicktime gets a unique name
-        info["resolvedPath"] = "shotgun_%s.mov" % uuid.uuid4().hex
+        info["resolvedPath"] = "%s.%s.mov" % (name, uuid.uuid4().hex)
+
         # If client override DL_PYTHON_HOOK_PATH env var, it changes the order python hook
         # are triggered and can change the value of the global hook useBackburnerPostExportAsset.
         # "useBackburner" bypass the global option and set the option for that specific export job.
         # use Flame to run the post-export callbacks (not backburner)
         info["useBackburner"] = False
-         
-        
+
     def populate_shotgun(self, session_id, info):
         """
         Flame hook called when an item has been exported.
@@ -199,14 +200,14 @@ class FlameReview(Application):
            versionNumber:   Current version number of export (0 if unversioned).
 
         """
-        
+
         if info.get("assetType") != "video":
             # the review system ignores any other assets. The export profiles are defined
             # in the app's settings hook, so technically there shouldn't be any other items
             # generated - but just in case there are (because of customizations), we'll simply
             # ignore these.
             return
-        
+
         # now typically quicktimes are generates as background jobs.
         # in that case, make sure our background job that we are submitting
         # to backburner gets executed *after* the quicktime generation has completed!
@@ -214,26 +215,26 @@ class FlameReview(Application):
             run_after_job_id = info.get("backgroundJobId")
         else:
             run_after_job_id = None
-        
+
         # set up the arguments which we will pass (via backburner) to 
         # the target method which gets executed
         args = {"info": info, "comments": self._review_comments}
-        
+
         # and populate UI params
-        
-        backburner_job_title = "%s %s - Shotgun Upload" % (self.get_setting("shotgun_entity_type"), 
-                                                                         info.get("sequenceName"))
-        backburner_job_desc = "Creates a new version record in Shotgun and uploads the associated Quicktime."        
-        
+
+        backburner_job_title = "%s %s - Shotgun Upload" % (self.get_setting("shotgun_entity_type"),
+                                                           info.get("sequenceName"))
+        backburner_job_desc = "Creates a new version record in Shotgun and uploads the associated Quicktime."
+
         # kick off async job
-        self.engine.create_local_backburner_job(backburner_job_title, 
-                                                backburner_job_desc, 
+        self.engine.create_local_backburner_job(backburner_job_title,
+                                                backburner_job_desc,
                                                 run_after_job_id,
-                                                self, 
+                                                self,
                                                 "backburner_populate_shotgun",
                                                 args,
                                                 info.get("destinationHost"))
-        
+
         # done!
         self._submission_done = True
 
@@ -277,24 +278,24 @@ class FlameReview(Application):
 
         :param comments: Review comments entered by the user.
          
-        """ 
-        
+        """
+
         full_path = os.path.join(info["destinationPath"], info["resolvedPath"])
 
         if not os.path.exists(full_path):
             raise TankError("Cannot find quicktime '%s'! Aborting upload." % full_path)
-        
+
         self.log_debug("Begin Shotgun processing for %s..." % full_path)
         self.log_debug("File size is %s bytes." % os.path.getsize(full_path))
-                
+
         # ensure that the entity exists in Shotgun
         entity_name = info["sequenceName"]
         entity_type = self.get_setting("shotgun_entity_type")
 
-        sg_data = self.shotgun.find_one(entity_type, [["code", "is", entity_name], 
-                                                      ["project", "is", self.context.project]]) 
-        
-        if not sg_data:    
+        sg_data = self.shotgun.find_one(entity_type, [["code", "is", entity_name],
+                                                      ["project", "is", self.context.project]])
+
+        if not sg_data:
             # Create a new item in Shotgun
             # First see if we should assign a task template
             # this is controlled via the app settings
@@ -303,17 +304,17 @@ class FlameReview(Application):
             self.log_debug("Creating a new item in Shotgun...")
             task_template_name = self.get_setting("task_template")
             task_template = None
-            if task_template_name: 
+            if task_template_name:
                 task_template = self.shotgun.find_one("TaskTemplate", [["code", "is", task_template_name]])
                 if not task_template:
                     raise TankError("The task template '%s' specified in the task_template setting "
                                     "does not exist!" % task_template_name)
-                
-            sg_data = self.shotgun.create(entity_type, {"code": entity_name, 
+
+            sg_data = self.shotgun.create(entity_type, {"code": entity_name,
                                                         "description": "Created by the Shotgun Flame integration.",
                                                         "task_template": task_template,
                                                         "project": self.context.project})
-            
+
             self.log_debug("Created %s" % sg_data)
 
             # upload thumb
@@ -333,7 +334,7 @@ class FlameReview(Application):
             title = "%s v%03d" % (info["sequenceName"], info["versionNumber"])
         else:
             title = info["sequenceName"]
-        
+
         data = {}
         data["code"] = title
         data["description"] = comments
@@ -341,7 +342,7 @@ class FlameReview(Application):
         data["entity"] = sg_data
         data["created_by"] = self.context.user
         data["user"] = self.context.user
-        
+
         # general metadata for the version
         # for the frame range, there isn't very meaningful metadata we can add
         # and we don't have corresponding frames on disk
@@ -357,20 +358,20 @@ class FlameReview(Application):
         data["sg_first_frame"] = 1
         data["sg_last_frame"] = info["sourceOut"] - info["sourceIn"]
         data["frame_count"] = info["sourceOut"] - info["sourceIn"]
-        data["frame_range"] = "%s-%s" % (data["sg_first_frame"], data["sg_last_frame"])         
+        data["frame_range"] = "%s-%s" % (data["sg_first_frame"], data["sg_last_frame"])
         data["sg_frames_have_slate"] = False
-        data["sg_movie_has_slate"] = False 
+        data["sg_movie_has_slate"] = False
         data["sg_frames_aspect_ratio"] = info["aspectRatio"]
         data["sg_movie_aspect_ratio"] = info["aspectRatio"]
-        
+
         # This is used to find the latest Version from the same department.
         # todo: make this configurable?
-        data["sg_department"] = "Editorial" 
-        
+        data["sg_department"] = "Editorial"
+
         sg_version_data = self.shotgun.create("Version", data)
-        
+
         self.log_debug("Created a version in Shotgun: %s" % sg_version_data)
-        
+
         # upload quicktime to Shotgun
         if self.get_setting("bypass_shotgun_transcoding"):
             self.log_debug("Begin upload of explicit mp4 quicktime to Shotgun...")
@@ -395,7 +396,7 @@ class FlameReview(Application):
             self.log_debug("Begin upload of quicktime to Shotgun...")
             self.shotgun.upload("Version", sg_version_data["id"], full_path, "sg_uploaded_movie")
             self.log_debug("Upload complete!")
-        
+
         # clean up
         try:
             self.log_debug("Trying to remove temporary quicktime file...")
@@ -457,8 +458,7 @@ class FlameReview(Application):
                     self.log_debug("Removed temporary file '%s'." % thumbnail_jpg)
                 except Exception, e:
                     self.log_warning("Could not remove temporary file '%s': %s" % (thumbnail_jpg, e))
-            
-            
+
     def display_summary(self, session_id, info):
         """
         Flame hook which is used to show summary UI to user
@@ -477,5 +477,3 @@ class FlameReview(Application):
         # pop up a UI asking the user for description
         tk_flame_review = self.import_module("tk_flame_review")
         self.engine.show_modal("Submission Summary", self, tk_flame_review.SummaryDialog, self._submission_done)
-        
-            

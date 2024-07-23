db8316154885c09eaa4d82da722a5dfbfb272ada
Julien Langlois <julien.langlois@autodesk.com>
2024-07-23 17:28:01 +0200

    Apply script - TODO CHECK THAT

--- python/tk_flame_review/ui/submit_dialog.py
+++ python/tk_flame_review/ui/submit_dialog.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'submit_dialog.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+## Created by: Qt User Interface Compiler version 5.15.16
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -17,45 +17,43 @@ for name, cls in QtGui.__dict__.items():
     if isinstance(cls, type): globals()[name] = cls
 
 
-
 from  . import resources_rc
 
 class Ui_SubmitDialog(object):
     def setupUi(self, SubmitDialog):
         if not SubmitDialog.objectName():
-            SubmitDialog.setObjectName("SubmitDialog")
+            SubmitDialog.setObjectName(u"SubmitDialog")
         SubmitDialog.resize(475, 559)
         self.verticalLayout = QVBoxLayout(SubmitDialog)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(20, -1, 20, -1)
         self.label = QLabel(SubmitDialog)
-        self.label.setObjectName("label")
-        self.label.setPixmap(QPixmap(":/tk-flame-review/ui_splash.png"))
+        self.label.setObjectName(u"label")
+        self.label.setPixmap(QPixmap(u":/tk-flame-review/ui_splash.png"))
 
         self.verticalLayout.addWidget(self.label)
 
         self.comments = QPlainTextEdit(SubmitDialog)
-        self.comments.setObjectName("comments")
+        self.comments.setObjectName(u"comments")
         self.comments.setMinimumSize(QSize(300, 100))
 
         self.verticalLayout.addWidget(self.comments)
 
         self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalSpacer = QSpacerItem(368, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.horizontalLayout.addItem(self.horizontalSpacer)
 
         self.cancel = QPushButton(SubmitDialog)
-        self.cancel.setObjectName("cancel")
+        self.cancel.setObjectName(u"cancel")
 
         self.horizontalLayout.addWidget(self.cancel)
 
         self.submit = QPushButton(SubmitDialog)
-        self.submit.setObjectName("submit")
+        self.submit.setObjectName(u"submit")
 
         self.horizontalLayout.addWidget(self.submit)
-
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
@@ -68,9 +66,8 @@ class Ui_SubmitDialog(object):
     # setupUi
 
     def retranslateUi(self, SubmitDialog):
-        SubmitDialog.setWindowTitle(QCoreApplication.translate("SubmitDialog", "Submit to Flow Production Tracking", None))
+        SubmitDialog.setWindowTitle(QCoreApplication.translate("SubmitDialog", u"Submit to Flow Production Tracking", None))
         self.label.setText("")
-        self.cancel.setText(QCoreApplication.translate("SubmitDialog", "Cancel", None))
-        self.submit.setText(QCoreApplication.translate("SubmitDialog", "Submit to Flow Production Tracking", None))
+        self.cancel.setText(QCoreApplication.translate("SubmitDialog", u"Cancel", None))
+        self.submit.setText(QCoreApplication.translate("SubmitDialog", u"Submit to Flow Production Tracking", None))
     # retranslateUi
-
--- python/tk_flame_review/ui/summary_dialog.py
+++ python/tk_flame_review/ui/summary_dialog.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'summary_dialog.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.15.2
+## Created by: Qt User Interface Compiler version 5.15.16
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -17,37 +17,36 @@ for name, cls in QtGui.__dict__.items():
     if isinstance(cls, type): globals()[name] = cls
 
 
-
 from  . import resources_rc
 
 class Ui_SummaryDialog(object):
     def setupUi(self, SummaryDialog):
         if not SummaryDialog.objectName():
-            SummaryDialog.setObjectName("SummaryDialog")
+            SummaryDialog.setObjectName(u"SummaryDialog")
         SummaryDialog.resize(501, 175)
         self.verticalLayout = QVBoxLayout(SummaryDialog)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(20, -1, 20, -1)
         self.stackedWidget = QStackedWidget(SummaryDialog)
-        self.stackedWidget.setObjectName("stackedWidget")
+        self.stackedWidget.setObjectName(u"stackedWidget")
         self.page = QWidget()
-        self.page.setObjectName("page")
+        self.page.setObjectName(u"page")
         self.verticalLayout_2 = QVBoxLayout(self.page)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.label = QLabel(self.page)
-        self.label.setObjectName("label")
-        self.label.setPixmap(QPixmap(":/tk-flame-review/submission_complete.png"))
+        self.label.setObjectName(u"label")
+        self.label.setPixmap(QPixmap(u":/tk-flame-review/submission_complete.png"))
 
         self.verticalLayout_2.addWidget(self.label)
 
         self.stackedWidget.addWidget(self.page)
         self.page_2 = QWidget()
-        self.page_2.setObjectName("page_2")
+        self.page_2.setObjectName(u"page_2")
         self.verticalLayout_3 = QVBoxLayout(self.page_2)
-        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.label_2 = QLabel(self.page_2)
-        self.label_2.setObjectName("label_2")
-        self.label_2.setPixmap(QPixmap(":/tk-flame-review/submission_failed.png"))
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setPixmap(QPixmap(u":/tk-flame-review/submission_failed.png"))
 
         self.verticalLayout_3.addWidget(self.label_2)
 
@@ -56,32 +55,28 @@ class Ui_SummaryDialog(object):
         self.verticalLayout.addWidget(self.stackedWidget)
 
         self.horizontalLayout = QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalSpacer = QSpacerItem(368, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.horizontalLayout.addItem(self.horizontalSpacer)
 
         self.submit = QPushButton(SummaryDialog)
-        self.submit.setObjectName("submit")
+        self.submit.setObjectName(u"submit")
 
         self.horizontalLayout.addWidget(self.submit)
 
-
         self.verticalLayout.addLayout(self.horizontalLayout)
-
 
         self.retranslateUi(SummaryDialog)
 
         self.stackedWidget.setCurrentIndex(0)
 
-
         QMetaObject.connectSlotsByName(SummaryDialog)
     # setupUi
 
     def retranslateUi(self, SummaryDialog):
-        SummaryDialog.setWindowTitle(QCoreApplication.translate("SummaryDialog", "Submit to Flow Production Tracking", None))
+        SummaryDialog.setWindowTitle(QCoreApplication.translate("SummaryDialog", u"Submit to Flow Production Tracking", None))
         self.label.setText("")
         self.label_2.setText("")
-        self.submit.setText(QCoreApplication.translate("SummaryDialog", "Ok", None))
+        self.submit.setText(QCoreApplication.translate("SummaryDialog", u"Ok", None))
     # retranslateUi
-
